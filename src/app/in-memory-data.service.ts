e3b79a4e3af5a69ed2839fe08c4abd5cba081bc3
BorgesAndrew <andrew.borges@avantis.edu.br>
2018-09-14 11:37:23 -0300

    Css Pesquisa

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,7 +5,7 @@ export class InMemoryDataService implements InMemoryDbService {
       const herois = [
         { id: 11, nome: 'João das Neves' },
         { id: 12, nome: 'Alan do Avell' },
-        { id: 13, nome: 'Bombasto' },
+        { id: 13, nome: 'Luiz Ditador De Noxus' },
         { id: 14, nome: 'Celeritas' },
         { id: 15, nome: 'Magneta' },
         { id: 16, nome: 'RubberMan' },
